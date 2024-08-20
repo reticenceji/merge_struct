4eab9210fe805fa205073302a0f8d93dae19337a
Ji Gaoqiang <reticenceji@qq.com>
2024-08-20 15:11:33 +0800

    update doc and syn version

--- merge_struct/src/lib.rs
+++ merge_struct/src/lib.rs
@@ -1,6 +1,7 @@
-pub use derive_merge::MergeProto;
+pub use derive_merge::Merge;
 
-/// The trait is designed for named struct.
+/// The trait is designed for named struct. Can be used to partially update configuration.
+///
 /// - If the field of struct has attribute `#[exclude]`, `this.field` remain the same.
 /// - ElIf the field of struct is not `Option`: `this.field = that.field.clone()`
 /// - ElIf the field of struct has attribute `#[force]`: `this.field = that.field.clone()`
@@ -11,8 +12,8 @@ pub use derive_merge::MergeProto;
 /// # Example
 ///
 /// ```rust
-/// use derive_merge_struct::MergeProto;
-/// #[derive(MergeProto)]
+/// use derive_merge_struct::Merge;
+/// #[derive(Merge)]
 /// #[exclude]
 /// struct TestStruct {
 ///     a: Option<i32>,
@@ -21,29 +22,47 @@ pub use derive_merge::MergeProto;
 ///     #[force]
 ///     d: Option<i32>,
 ///     #[exclude]
-///     e: Option<i32>
+///     e: Option<i32>,
+///     f: i32
 /// }
 /// let mut this = TestStruct {
 ///     a: Some(1),
 ///     b: None,
 ///     c: Some(1),
 ///     d: Some(1),
-///     e: None
+///     e: None,
+///     f: 1
 /// };
 /// let that = TestStruct {
 ///     a: Some(2),
 ///     b: Some("hello".to_string()),
 ///     c: None,
 ///     d: None,
-///     e: Some(1)
+///     e: Some(1),
+///     f: 2
 /// };
-/// this.merge_proto(&that);
+/// this.merge(that);
 /// assert_eq!(this.a, Some(2));
 /// assert_eq!(this.b, Some("hello".to_string()));
 /// assert_eq!(this.c, Some(1));
 /// assert_eq!(this.d, None);
 /// assert_eq!(this.e, None);
+/// assert_eq!(this.f, 2);
 /// ```
-pub trait MergeProto {
-    fn merge_proto(&mut self, another: &Self);
+pub trait Merge {
+    fn merge(&mut self, another: Self);
 }
+
+// #[allow(unused)]
+// #[derive(Merge)]
+// #[exclude]
+// struct TestStruct {
+//     a: Option<i32>,
+//     b: Option<String>,
+//     c: Option<u32>,
+//     #[force]
+//     d: Option<i32>,
+//     #[exclude]
+//     e: Option<i32>,
+//     f: i32,
+// }
